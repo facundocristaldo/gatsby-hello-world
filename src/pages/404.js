0a7e23532299a35d8a82186f865961a7ac217814
Facundo Cristaldo <facu.cristaldo.96@gmail.com>
2021-07-08 08:10:53 -0300

    first commit

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      Page not Found
+    </div>
+  )
+}
+
+export default NotFoundPage
---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>Abuot page</h1>
+      <p>some lorem text</p>
+      <p>some lorem text</p>
+      <p>some lorem text</p>
+    </div>
+  )
+}
+
+export default About
